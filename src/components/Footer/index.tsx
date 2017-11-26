62e805e3cab78aab2bd14354237848666f67c670
Joseph Duffy <josephduffy@me.com>
2017-11-26 14:26:01 +0000

    Updates "Baking Votes" to "Baking Feedback"

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ export default class Footer  extends React.Component {
   public render() {
     return (
     <footer>
-      Created by <a href="https://josephduffy.co.uk" title="Visit Joseph Duffy's website">Joseph Duffy</a>. <a href="https://github.com/JosephDuffy/baking-votes" title="View the source code on GitHub">Source available on GitHub</a>.
+      Created by <a href="https://josephduffy.co.uk" title="Visit Joseph Duffy's website">Joseph Duffy</a>. <a href="https://github.com/JosephDuffy/baking-feedback" title="View the source code on GitHub">Source available on GitHub</a>.
     </footer>
     );
   }
