9489a810d1747be7f2e916d236630d80397e0466
Joseph Duffy <josephduffy@me.com>
2017-12-14 20:32:02 +0000

    Fixes bug causing redux-persist to only persist first state change

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,52 +1,25 @@
 import * as localForage from 'localforage';
 import { combineReducers } from 'redux';
-import { createTransform, persistReducer } from 'redux-persist';
+import { persistReducer } from 'redux-persist';
 
 import surveyAnswers, { State as SurveysAnswersState } from './surveyAnswers';
 import { default as surveysReducer, State as SurveysState } from './surveys';
+
+// TODO: Reset surveys' `loadError` and `loading` properties on rehydration
 
 export type AppState = {
   surveys: SurveysState,
   surveyAnswers: SurveysAnswersState,
 };
 
-const surveysPersistTransforms = createTransform(
-  // transform state coming from redux on its way to being serialized and stored
-  (state: SurveysState) => {
-    return state;
-  },
-  // transform state coming from storage, on its way to be rehydrated into redux
-  (state: any, key: string) => {
-    if (key === 'surveys') {
-      const surveys = {};
-
-      for (const surveyId of Object.keys(state)) {
-        const survey = state[surveyId];
-        delete survey.loadError;
-        survey.loading = false;
-        surveys[surveyId] = survey;
-      }
-
-      return surveys;
-    }
-    return state;
-  },
-);
-
-const surveysPersistConfig = {
-  key: 'surveys',
-  storage: localForage,
-  transforms: [
-    surveysPersistTransforms,
-  ],
-};
-
-const surveyAnswersPersistConfig = {
-  key: 'surveyAnswers',
-  storage: localForage,
-};
-
-export default combineReducers({
-  surveys: persistReducer(surveysPersistConfig, surveysReducer),
-  surveyAnswers: persistReducer(surveyAnswersPersistConfig, surveyAnswers),
+const combinedReducers = combineReducers<AppState>({
+  surveys: surveysReducer,
+  surveyAnswers,
 });
+
+const config = {
+  key: 'app',
+  storage: localForage,
+};
+
+export default persistReducer(config, combinedReducers);
