685de188e8613b7201d03778e2ccd75c6a4d7591
Joseph Duffy <josephduffy@me.com>
2018-01-24 20:08:04 +0000

    Updates API URL

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ const apiBaseURL = (() => {
   if (hostname === 'localhost' || hostname === '127.0.0.1') {
     return 'http://localhost:13515';
   } else {
-    return 'https://api.bakingvotes.com';
+    return 'https://api.bakingfeedback.com';
   }
 })();
 
