4e1f1347cecfce7a2f920d85c10e556e7bfc092b
Joseph Duffy <josephduffy@me.com>
2018-03-18 17:28:35 +0000

    Adds transform to state persistence to clear `loading` and `errors` keys from `surveys`

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,6 @@
 import * as localForage from 'localforage';
 import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
+import { createTransform, persistReducer } from 'redux-persist';
 import immutableTransform from 'redux-persist-transform-immutable';
 
 import surveyAnswers, { State as SurveysAnswersState } from './surveyAnswers';
@@ -15,6 +15,25 @@ export type AppState = {
   surveyResults: SurveysResultsState,
 };
 
+const surveysPersistTransforms = createTransform(
+  // transform state coming from redux on its way to being serialized and stored
+  (state: SurveysState) => {
+    return state;
+  },
+  // transform state coming from storage, on its way to be rehydrated into redux
+  (state: any, key: string) => {
+    if (key === 'surveys') {
+      let transformedState = state;
+
+      transformedState = transformedState.get('loading').clear();
+      transformedState = transformedState.get('errors').clear();
+
+      return transformedState;
+    }
+    return state;
+  },
+);
+
 const combinedReducers = combineReducers<AppState>({
   surveys: surveysReducer,
   surveyAnswers,
@@ -24,7 +43,10 @@ const combinedReducers = combineReducers<AppState>({
 const config = {
   key: 'app',
   storage: localForage,
-  transforms: [immutableTransform()],
+  transforms: [
+    surveysPersistTransforms,
+    immutableTransform(),
+  ],
 };
 
 export default persistReducer(config, combinedReducers);
